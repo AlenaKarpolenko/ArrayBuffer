347063487fcb80b5660978f1cfabd28404ff8155
AlenaKarpolenko <alenac190590@mail.ru>
2022-07-29 22:31:45 +0700

    added

--- ci-template/src/js/Magician.js
+++
@@ -1,9 +0,0 @@
-import Character from './app';
-
-export default class Magician extends Character {
-  constructor(name, type) {
-    super(name, type);
-    this.attack = 100;
-    this.stoned = false;
-  }
-}
--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -1,29 +1,12 @@
-export default class Character {
-	constructor(name, type) {
-	  this.name = name;
-	  this.type = type;
-	  this.distance = 1;
-	  this.stoned = false;
-	}
+export default function getBuffer() {
+  const data = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  return ((input) => {
+	  const buffer = new ArrayBuffer(data.length * 2);
+	  const bufferView = new Uint16Array(buffer);
 
-	set stoned(value) {
-	  this.stone = value;
-	}
-	set attack(value) {
-	  this.attacked = value;
-	}
-
-	get stoned() {
-	  return this.stone;
-	}
-
-	get attack() {
-	  const attack = this.attacked * (1 - (this.distance - 1) / 10);
-	  if (this.stone) {
-		return attack - Math.log2(this.distance) * 5;
+	  for (let i = 0; i < input.length; i += 1) {
+      bufferView[i] = input.charCodeAt(i);
 	  }
-	  return attack;
-	}
-  }
-
-  //Возможность насылать «дурман» на магов и демонов. При это их сила атаки падает уже не только линейно, а по формуле: attack - log2(x) * 5, где x - это расстояние в клетках. attack рассчитывается с учётом п.1. Пример: атака 100 единиц, атакуем 2 клетку от себя, получаем вместе с дурманом: 85 (вместо 90).
+	  return buffer;
+  })(data);
+}
